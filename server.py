c7d7d150c9568dcc7d9b6601f6e428834ac9b41b
nghiahuynhtv01042002 <nghiahuynhtv01042002.com>
2024-04-22 19:09:54 +0700

    add ip

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # listen conect to port 12345
 # server_socket.bind(('127.0.0.1', 12345))
 # server_socket.bind(('localhost', 12345))
-server_socket.bind(('192.168.31.252', 12345))
-
+# server_socket.bind(('192.168.31.252', 12345))
+server_socket.bind(('192.168.207.70', 12345))
 server_socket.listen(5)
 
 print("Server is listening...")
