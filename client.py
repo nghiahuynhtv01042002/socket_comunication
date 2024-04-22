ce33cd4aacf85898237ed2563fa422d5880df58b
nghiahuynhtv01042002 <nghiahuynhtv01042002.com>
2024-04-22 12:04:43 +0700

    third commit

--- client.py
+++ client.py
@@ -1,23 +1,23 @@
 import socket
 import time
-# Tạo một socket object
+# create socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Kết nối tới server
+# conect to server
 client_socket.connect(('192.168.31.252', 12345))
 count = 0
 while True:
-    # Nhập dữ liệu từ người dùng
+    # enter message
     message = "I Am TXN\n"
-    # Gửi dữ liệu tới server
+    # send data to server
     client_socket.send(message.encode())
     
-    # Nhận dữ liệu từ server
+    # recieve data from server
     data = client_socket.recv(1024).decode()
     # print(f"Received from server: {data} {count}")
     print("Received from server: {} {}".format(data,count))
     count+=1
     time.sleep(2)
 
-# Đóng kết nối
+#close conection
 client_socket.close()
--- server.py
+++ server.py
@@ -2,10 +2,10 @@ import socket
 import time
 
 count = 0
-# Tạo một socket object
+#create socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Lắng nghe các kết nối đến ở cổng 12345
+# listen conect to port 12345
 # server_socket.bind(('127.0.0.1', 12345))
 # server_socket.bind(('localhost', 12345))
 server_socket.bind(('192.168.31.252', 12345))
@@ -14,14 +14,14 @@ server_socket.listen(5)
 
 print("Server is listening...")
 
-# Chấp nhận kết nối từ client
+# accept conection from client
 client_socket, addr = server_socket.accept()
 
 # print(f"Connection from {addr} has been established.")
 print("Connection from {} has been established.".format(addr))
-# tạo hàm handle client ở đây
+# making handle client here
 while True:
-    # Nhận dữ liệu từ client
+    # recieve data from client
     data = client_socket.recv(1024).decode().strip()
     if not data:
         break
@@ -29,11 +29,11 @@ while True:
     print("Received from client: {} {}".format(data,count))
     count +=1
  
-    # Gửi dữ liệu lại cho client
+    # send back data to client
     data_send = "" + data
     client_socket.send(data_send.encode())
     time.sleep(2)
 
-# Đóng kết nối
+# close conect
 client_socket.close()
 server_socket.close()
